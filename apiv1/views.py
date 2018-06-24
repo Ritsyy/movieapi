30bdcbf5b0693b7a2fc0147d7af67af309d53d8d
Ritsyy <richarupela95@gmail.com>
2018-06-25 00:57:00 +0530

    add update and delete API

--- apiv1/views.py
+++ apiv1/views.py
@@ -12,7 +12,7 @@ from .serializers import MovieSerializer
 
 
 class MovieAPIView(APIView):
-    allowed_methods = ['GET']
+    allowed_methods = ['GET', 'POST', 'PUT']
     serializer_class = MovieSerializer
     pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
 
@@ -39,3 +39,44 @@ class MovieAPIView(APIView):
             'request': request
         })
         return paginator.get_paginated_response(serializer.data)
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        genres = request.data.get('genres')
+        if serializer.is_valid():
+            movie = serializer.save()
+            for item in genres:
+                genre_name = item.strip()
+                genre, created = Genre.objects.get_or_create(name=genre_name)
+                movie.genre.add(genre)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        movie_id = request.data.get('id')
+        movie = Movie.objects.filter(id=movie_id).first()
+        if not movie:
+            # If movie does not exist, create it
+            self.post(request)
+        # Update the movie
+        movie.name = request.data.get('name')
+        movie.popularity = request.data.get('popularity')
+        movie.score = request.data.get('score')
+        movie.director = request.data.get('director')
+        movie.save()
+        genres = request.data.get('genres')
+        movie.genre.all().delete()  # clear existing genres for this movie
+        for item in genres:
+            genre_name = item.strip()
+            genre, created = Genre.objects.get_or_create(name=genre_name)
+            movie.genre.add(genre)
+        return Response({'message': "updated movie"}, status=status.HTTP_200_OK)
+
+    def delete(self, request):
+        movie_id = request.data.get('id')
+        movie = Movie.objects.filter(id=movie_id).first()
+        if not movie:
+            return Response({'message': "not found"}, status=status.HTTP_400_BAD_REQUEST)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
